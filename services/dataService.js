10a90b1db6afe2e7d4d3f4ade13896ae764c76e9
ColinOchs <97154394+ColinOchs@users.noreply.github.com>
2022-03-11 19:21:34 -0800

    Add files via upload

--- services/dataService.js
+++ services/dataService.js
@@ -1,52 +1,102 @@
 var catalog = [
     {
-        _id: "fhklajshdflkjashdf",
-        title: "farfenugen",
-        price: 12.99,
+        _id: "fhklajshdf3456lkja7shdf",
+        title: "polka dots",
+        price: 7.99,
         stock: 34,
         category: "stationary",
-        image: "https://picsum.photos/200/300",
+        image: "1-letter.jpg",
         discount: 4,
         
     },
     {
-        _id: "glgkdjlgkjlkjdf",
-        title: "garbanzo",
+        _id: "glgkdj34568lgkjlkjdf",
+        title: "assorted prints",
         price: 24.99,
         stock: 23,
         category: "stationary",
-        image: "https://picsum.photos/200/300",
+        image: "2-letter.jpg",
         discount: 2,
     },
     {
-        _id: "wasdfalglkjg",
-        title: "winkerbean",
+        _id: "wasdfalglkj3456g",
+        title: "black and white prints",
         price: 23.99,
         stock: 13,
-        category: "stationary",
-        image:"https://picsum.photos/200/300",
+        category: "calendars",
+        image:"1-calendar.jpg",
         discount: 4,
     },
     {
-        _id: "alsdkjflkdjasflkj",
-        title: "cockatoo",
-        price: 132.99,
+        _id: "alsdkjflkdjasflk87987j",
+        title: "color prints",
+        price: 32.99,
         stock: 4,
-        category: "stationary",
-        image: "https://picsum.photos/200/300",
+        category: "calendars",
+        image: "2-calendar.jpg",
         discount: 0,
         
     },
     {
-        _id: "oieurohisdfoh",
-        title: "nice-a-roni",
-        price: 19.99,
+        _id: "oieurohis586785687dfoh",
+        title: "bird prints",
+        price: 29.99,
         stock: 34,
-        category: "calendar",
-        image: "https://picsum.photos/200/300",
+        category: "calendars",
+        image: "3-calendar.jpg",
         discount: 10,
         
     },
+    {
+        _id: "oieur3456ohisdfoh",
+        title: "calendar stand",
+        price: 36.99,
+        stock: 34,
+        category: "calendars",
+        image: "4-calendar.jpg",
+        discount: 10,
+        
+    },
+    {
+        _id: "oieurohisdfo4356h",
+        title: "alphabet print",
+        price: 9.99,
+        stock:54,
+        category: "framed prints",
+        image: "1-frame.jpg",
+        discount: 10,
+        
+    },
+    {
+        _id: "oieurohisdf3245oh",
+        title: "fish print",
+        price: 9.99,
+        stock: 44,
+        category: "framed prints",
+        image: "2-frame.jpg",
+        discount: 10,
+        
+    },
+    {
+        _id: "oieurohis5646dfoh",
+        title: "fox print",
+        price: 9.99,
+        stock: 36,
+        category: "framed prints",
+        image: "3-frame.jpg",
+        discount: 10,
+        
+    },
+    {
+        _id: "oieuroh34534isdfoh",
+        title: "location print",
+        price: 9.99,
+        stock: 26,
+        category: "framed prints",
+        image: "6-frame.jpg",
+        discount: 10,
+        
+    }
 ];
 
 
