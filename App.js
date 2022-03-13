6bd27aeac3f73f797fb9d826e8b85453e6a7e4e7
ColinOchs <97154394+ColinOchs@users.noreply.github.com>
2022-03-12 20:04:33 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import Footer from './components/footer';
 import QuantityPicker from './components/quantityPicker';
 import Catalog from './components/catalog';
 import Product from './components/product';
+import Header from './components/header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 
 function App() {
   return ( <div className="App">
+        <Header></Header>
         <NavBar></NavBar>
           <h1>Greetings and welcome to your home for creative design solutions</h1>
           <Catalog></Catalog>
---
+++ footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer  = () => {
+    return (<div className="footer">
+        <h2>&copy; 2022, ColinTech Industries</h2><h4><p>Developer: Colin Ochs</p></h4><h5>&</h5>
+        
+    </div>);
+};
+
+export default Footer;
+
